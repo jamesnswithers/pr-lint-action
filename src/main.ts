a5d74dcac5c0b23fea66edbc49f70d85f1213ab9
James Withers <jamesnswithers@gmail.com>
2020-04-26 11:33:37 +0100

    updated to remove auto-close and instead create a comment on the PR

--- src/main.ts
+++ src/main.ts
@@ -3,42 +3,23 @@ import * as github from '@actions/github';
 
 async function run() {
   try {
+    const context = github.context;
     const
       titleRegex = core.getInput('title-regex', {required: true}),
       titleRegexFlags = core.getInput('title-regex-flags') || 'g',
-      title = github.context!.payload!.pull_request!.title;
+      title = context!.payload!.pull_request!.title;
 
     core.info(`Checking "${titleRegex}" with "${titleRegexFlags}" flags against the PR title: "${title}"`);
 
     if (!title.match(new RegExp(titleRegex, titleRegexFlags))) {
-      core.setFailed(`Please fix your PR title to match "${titleRegex}" with "${titleRegexFlags}" flags, and re-trigger the check by pushing a new commit. See https://github.com/seferov/pr-lint-action#known-issues`);
+      core.setFailed(`Please fix your PR title to match "${titleRegex}" with "${titleRegexFlags}" flags, and re-trigger the check by pushing a new commit.`);
 
-      const autoCloseMessage = core.getInput('auto-close-message'),
-        githubToken = core.getInput('github-token');
-      if (autoCloseMessage) {
-        if (!githubToken) {
-          core.setFailed('To use auto-close feature you must provide github-token. See: https://github.com/seferov/pr-lint-action#auto-close');
+      const github_token = core.getInput('GITHUB_TOKEN');
 
-          return;
-        }
-
-        const client: github.GitHub = new github.GitHub(githubToken),
-        pr = github.context.issue;
-
-        client.pulls.createReview({
-          owner: pr.owner,
-          repo: pr.repo,
-          pull_number: pr.number,
-          body: autoCloseMessage.replace('%pattern%', titleRegex),
-          event: 'COMMENT'
-        });
-        client.pulls.update({
-          owner: pr.owner,
-          repo: pr.repo,
-          pull_number: pr.number,
-          state: 'closed'
-        });
-      }
+      const pull_request_number = context.payload.pull_request.number;
+      const octokit = new github.GitHub(github_token);
+      const message = "Pull Request title validation Failed";
+      const new_comment = octokit.issues.createComment(Object.assign(Object.assign({}, context.repo), { issue_number: pull_request_number, body: message }));
     }
   } catch (error) {
     core.setFailed(error.message);
