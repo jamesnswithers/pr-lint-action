c0cc2d8bb5f652ff1095c3aa0d277b954e2c24ea
James Withers <JamesWithers@hastingsdirect.com>
2020-05-10 12:41:50 +0100

    amended dependency import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core';
-import * as github from 'github-script';
+import * as github from '@actions/github';
 import { getRequiredEnvironmentVariable } from "./utils";
 import { States } from "./statusStates";
 
